e5a427fc2b4f88b6bd725c264b4c2c89416ab0fd
Farhan <farhan-HP-ProBook-4430s>
2013-09-04 01:03:54 +0600

    add some methods

--- DsAndAlgorithms/src/com/ff/linkedlist/SingleLinkedList.java
+++ DsAndAlgorithms/src/com/ff/linkedlist/SingleLinkedList.java
@@ -7,22 +7,97 @@ public class SingleLinkedList
 	
 	public SingleLinkedList() 
 	{
-		size = 0;		
+		size = 0;
+		head = null;
 	}
 	
 	public void addFirst(int data)
 	{
+		SingleLinkedNode node = null;
+		node = new SingleLinkedNode();
+		node.data = data;
+		
+		if(head == null)
+		{	
+			node.next = null;
+			head = node;
+		}else
+		{
+			node.next = head;
+			head = node;			
+		}
 		
 	}
 	
 	public void addLast(int data)
 	{
+		SingleLinkedNode node = new SingleLinkedNode();
+		node.data = data;
 		
+		if(head != null)
+		{
+			SingleLinkedNode temp = head; 
+			
+			while (temp != null) 
+			{
+				if(temp.next == null)
+				{
+					temp.next = node;
+					break;
+				}
+				temp = temp.next;
+			}
+		}else 
+		{
+			node.next = null;
+			head = node;
+		}
 	}
 	
 	public void addBetween(int data,int pos)
 	{
 		
+	}
+	
+	public boolean removeFirst()
+	{
+		if(head != null)
+		{
+			SingleLinkedNode temp = head;
+			head = head.next;
+			temp = null;
+			return true;
+		}		
+		return false;
+	}
+	
+	public void removeLast()
+	{
+		
+	}
+	
+	public void remove(int pos)
+	{
+		
+	}
+	
+	public int findPos()
+	{
+		return 0;
+	}
+	
+	public void show()
+	{
+		if(head != null)
+		{
+			SingleLinkedNode temp = head;
+			while (temp != null)
+			{
+				System.out.print(temp.data+" ");
+				temp = temp.next;				
+			}
+			//System.out.print(temp.data+" ");
+		}
 	}
 
 	public int getSize() {
--- DsAndAlgorithms/src/com/ff/linkedlist/SingleLinkedListTest.java
+++ DsAndAlgorithms/src/com/ff/linkedlist/SingleLinkedListTest.java
@@ -2,4 +2,16 @@ package com.ff.linkedlist;
 
 public class SingleLinkedListTest {
 
+	public static void main(String args[])
+	{
+		SingleLinkedList linkedList = new SingleLinkedList();
+		linkedList.addFirst(10);
+		linkedList.addFirst(20);
+		linkedList.addLast(50);
+
+		linkedList.removeFirst();
+		linkedList.show();
+
+
+	}
 }
