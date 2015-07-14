1293b84e9d1b4dcf6ed6683c89d6c2f66d5a7d8d
Farhan Faruque <russel.cse05@gmail.com>
2015-07-14 07:17:16 +0600

    mergesort

---
+++ Java/algorithms/src/me/farhan/sorting/Comparable.java
@@ -0,0 +1,8 @@
+package me.farhan.sorting;
+
+/**
+ * Created by farhan on 7/14/15.
+ */
+public interface Comparable<T> {
+    void compareTo(T t);
+}
---
+++ Java/algorithms/src/me/farhan/sorting/MergeSort.java
@@ -0,0 +1,16 @@
+package me.farhan.sorting;
+
+/**
+ * Created by farhan on 7/14/15.
+ */
+public class MergeSort {
+    
+    public MergeSort(Comparable [] data)
+    {
+
+    }
+
+    public static void sort(Comparable [] data){
+
+    }
+}
